3d52597f81e44c696db1dceee2ea6463fe53b051
ymcheung <ymcheung.tw@me.com>
2022-04-28 10:21:07 +0800

    Add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,20 @@
 import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
-// Full Astro Configuration API Documentation:
+import preact from '@astrojs/preact'; // Full Astro Configuration API Documentation:
 // https://docs.astro.build/reference/configuration-reference
-
 // @type-check enabled!
 // VSCode and other TypeScript-enabled text editors will provide auto-completion,
 // helpful tooltips, and warnings if your exported object is invalid.
 // You can disable this by removing "@ts-check" and `@type` comments below.
 
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
 export default defineConfig({
   outDir: './dist',
-  integrations: [preact()],
+  integrations: [preact(), sitemap()],
   site: 'https://moment.intersection.tw',
   trailingSlash: 'never',
-  markdown: { mode: 'mdx' }
+  markdown: {
+    mode: 'mdx'
+  }
 });
